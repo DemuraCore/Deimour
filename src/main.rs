71ef589f2861cdff50943b678cd2f2f3bb2c6149
DemuraAIdev <cyberaioff@gmail.com>
2024-11-16 19:26:44 +0700

    Add ASCII art banner and version information to main

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,21 @@ use handler::Handler;
 
 static START: OnceLock<Instant> = OnceLock::new();
 
+const OP_TEXT: &str = r"
+  _____  ______ _____ __  __  ____  _    _ _____  
+ |  __ \|  ____|_   _|  \/  |/ __ \| |  | |  __ \ 
+ | |  | | |__    | | | \  / | |  | | |  | | |__) |
+ | |  | |  __|   | | | |\/| | |  | | |  | |  _  / 
+ | |__| | |____ _| |_| |  | | |__| | |__| | | \ \ 
+ |_____/|______|_____|_|  |_|\____/ \____/|_|  \_\                                       
+
+ VERSION 0.1.0
+";
+
 #[tokio::main]
 async fn main() {
+    println!("{}", OP_TEXT);
+
     START.get_or_init(|| Instant::now());
     dotenv::dotenv().expect("Failed to load .env file");
 
